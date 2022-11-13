1d820326a2bb193a09a15654352c82538e3316cd
silaksila <silaksila27@gmail.com>
2022-11-13 21:28:09 +0100

    yea

--- bot_v2_.py
+++ bot_v2_.py
@@ -3,7 +3,7 @@ import threading
 import requests
 import json
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ConversationHandler, MessageHandler
 from telegram.constants import ParseMode
 
 logging.basicConfig(
@@ -35,27 +35,57 @@ async def crypto(update: Update, context: ContextTypes.DEFAULT_TYPE):
     crypto = data["crypto"]["coins"]
     crypto_amounts = list(crypto.values())
     crypto_names = list(crypto.keys())
-    # get crypto price from binance api
-    urls = [key+name+"USDT" for name in crypto_names]
-    prices = [requests.get(url).json() for url in urls]
-    btc_price = float(prices[0]["price"])
-    string = ""
+
     if data["crypto"]["setup"]:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="*Current state:* \n", parse_mode=ParseMode.MARKDOWN_V2)
+        # get crypto price from binance api
+        urls = [key+name+"USDT" for name in crypto_names]
+        prices = [requests.get(url).json() for url in urls]
+
+        crypto_profile_string = ""
+        curr_state_string = ""
         for indx, val in enumerate(prices):
             crypto_price = float(val["price"])
             difference_to_old_price = (
                 (crypto_price/crypto_amounts[indx][1])-1)*100
-            string += (
+            curr_state_string += (
                 f"{crypto_names[indx]} : {crypto_price:,.2f} $  {difference_to_old_price:+.2f} %\n")
+
+            crypto_profile_string += (
+                f"{crypto_names[indx]} : {crypto_amounts[indx][0]*crypto_price:.2f}. $ \n")
             data["crypto"]["coins"][crypto_names[indx]][1] = crypto_price
 
-        with open("database.json", "w") as file:
+        with open("database.json", "w") as file:  # write new price values to later compare to
             json.dump(data, file)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=string.replace(",", " ").replace(".", ","))
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Your crypto profile:\n {crypto_names[0]} : {crypto_amounts[0][0]*btc_price:.2f}. $ ".replace(".", ","))
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="*Current state:* \n", parse_mode=ParseMode.MARKDOWN_V2)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=curr_state_string.replace(",", " ").replace(".", ","))
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=f"*Your crypto profile:*\n ", parse_mode=ParseMode.MARKDOWN_V2)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=crypto_profile_string.replace(",", " ").replace(".", ","))
     else:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Your crypto profile isn't set up, do you wish to set it up right now ?Y/N")
+
+
+async def crypto_set_up_profile_start():
+    pass
+
+
+async def crypto_set_up_profile_not():
+    pass
+
+
+async def stop_crypto_set_up():
+    pass
+
+set_up_crypto_profile = ConversationHandler(
+    entry_points=[
+        MessageHandler("N", crypto_set_up_profile_not),
+        MessageHandler("n", crypto_set_up_profile_not),
+        MessageHandler("Y", crypto_set_up_profile_start),
+        MessageHandler("y", crypto_set_up_profile_start)],
+    states={},  # TODO
+    fallbacks=[CommandHandler("stop"), stop_crypto_set_up]
+)
 
 
 if __name__ == '__main__':
